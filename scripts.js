1b59947b8366a178927e66b4a548bb3bd08f6c97
franksara8 <66429804+franksara8@users.noreply.github.com>
2020-06-12 16:04:02 +0200

    finished

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
 
-$('button').click(() =>{
-    $('ul').append(`<li class="list">`+$('input').val()+`</li>`);
-});
-
-$('li').click( () => {
-    $('li').css('text-decoration', 'line-through');
-});
+// $(document).ready(function(){
+    $('.add').click(() =>{
+        let val = $('input').val();
+        if ( val != '') {
+            $('ul').append(`<li class="list">`+$('input').val()+`<div class="buttons"><button class="trashcan"><img src="trashcan.png"></button><button class="tick"><img src="tick.png" alt=""></button></div></li>`);
+          
+        } else {
+            alert ("Please add a valid todo!")
+        }
+    });
+    $('ul').on('click', 'button.trashcan' , function() {
+        $(this).parents().eq(1).remove();
+    });
+    $('ul').on('click', 'button.tick' , function() {
+        $(this).parents().eq(1).css('text-decoration', 'line-through');
+    });
+// });
 
 
